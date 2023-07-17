bc2dd504c9e2b63bcb194b924de00cc81bc30be9
Dakota Shively <122128148+DakotaShively@users.noreply.github.com>
2023-07-17 17:07:04 -0400

    New slides

---
+++ Little Lemon/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Little Lemon
+//
+//  Created by Dakota Shively on 7/17/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
